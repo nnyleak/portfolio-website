65085f5c2e13f0fc7a43735ac7797982177f4166
Kaelynn Amodia <89933890+nnyleak@users.noreply.github.com>
2025-04-03 16:21:57 -0400

    i finally got the carousel to work YESSSSSSSSSSSS

--- src/project-page.js
+++ src/project-page.js
@@ -13,31 +13,31 @@
 // });
 
 // initialize swiper carousel
+// const swiper = new Swiper(".swiper", {
+//   grabCursor: true,
+//   centeredSlides: true,
+//   initialSlide: 0,
+//   speed: 600,
+//   preventClicks: true,
+//   slidesPerView: "auto",
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+// });
+
 const swiper = new Swiper(".swiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  initialSlide: 1,
-  speed: 600,
-  preventClicks: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 80,
-    depth: 350,
-    modifier: 1,
-    slideShadows: true,
-  },
-  on: {
-    click(event) {
-      swiper.slideTo(this.clickedIndex);
-    },
-  },
+  // pagination
   pagination: {
     el: ".swiper-pagination",
   },
+
+  // navigation
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-});
+})
